c3c59cdcd1822a2858a89f7389bc7f05fad5aa13
Kevin B. Adesara <kevin.adesara@comp91.digi-corp.com>
2016-05-12 16:05:50 +0530

    dbmanager changes and test case changes from miralmegha-master

--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
 var assert = require('chai').assert;
 var debug = require('debug')('sc-core:test');
+
+
+
 describe('Database', function() {
   // 1. Check database connection
   describe('check connection', function() {
@@ -14,6 +17,7 @@ describe('Database', function() {
           done(err);
           return;
         }
+        conn.release();
         done();
       });
     });
@@ -34,6 +38,139 @@ describe('Database', function() {
   }); // End #createTable()
 
   // 3. Check insert
+  // describe('create insert function', function() {
+  //    it('should  insert data', function(done) {
+  //      var DatabaseManager = require('../lib/database/database-manager.js');
+  //      DatabaseManager.insert(1,'HTML & CSS',function(res) {
+  //        if (res.status === false) {
+  //          done(new Error(res.error.message));
+  //          return;
+  //        }
+  //        DatabaseManager.insert(67,'Anguler JS',function(res) {
+  //          if (res.status === false) {
+  //            done(new Error(res.error.message));
+  //            return;
+  //          }
+  //          done();
+  //        });
+  //
+  //
+  //      }); // END create table
+  //    }); // END test case
+  //  });
+
+   // 4. Check delete
+  //  describe('create delete function', function() {
+  //     it('should  delete data', function(done) {
+  //       var DatabaseManager = require('../lib/database/database-manager.js');
+  //       DatabaseManager.delete(53,function(res) {
+  //         if (res.status === false) {
+  //           done(new Error(res.error.message));
+  //           return;
+  //         }
+  //         done();
+  //       }); // END create table
+  //     }); // END test case
+  //   });
+
+
+  //4. Check Selection
+   describe('create Selection function', function() {
+      it('should  Select data', function(done) {
+        var DatabaseManager = require('../lib/database/database-manager.js');
+        DatabaseManager.select(0,function(res) {
+          if (res.status === false) {
+            done(new Error(res.error.message));
+            return;
+          }
+          done();
+        }); // END create table
+      }); // END test case
+    });
+
+    //5. Check Selection Subtree
+     /*describe('create Selection Subtree function', function() {
+        it('should  Select Subtree data', function(done) {
+          var DatabaseManager = require('../lib/database/database-manager.js');
+          DatabaseManager.selectsubtree(67,function(res) {
+            if (res.status === false) {
+              done(new Error(res.error.message));
+              return;
+            }
+            done();
+          }); // END create table
+        }); // END test case
+      });*/
+
+      //6. Check Selection Subtree
+/*       describe('create Selection Subtree function', function() {
+          it('should  Select Subtree data', function(done) {
+            var DatabaseManager = require('../lib/database/database-manager.js');
+            DatabaseManager.selectsubtree(67,function(res) {
+              if (res.status === false) {
+                done(new Error(res.error.message));
+                return;
+              }
+              done();
+            }); // END create table
+          }); // END test case
+        });*/
+
+        //7. Check Selection Subtree Depth
+        /* describe('create Selection Subtree Depth function', function() {
+            it('should  Select Subtree Depth data', function(done) {
+              var DatabaseManager = require('../lib/database/database-manager.js');
+              DatabaseManager.selectsubtreedepth(function(res) {
+                if (res.status === false) {
+                  done(new Error(res.error.message));
+                  return;
+                }
+                done();
+              }); // END create table
+            }); // END test case
+          });*/
+          //8. Check Tree Path
+           /*describe('create Tree Path function', function() {
+              it('should  Tree Path  data', function(done) {
+                var DatabaseManager = require('../lib/database/database-manager.js');
+                DatabaseManager.treepath(71,function(res) {
+                  if (res.status === false) {
+                    done(new Error(res.error.message));
+                    return;
+                  }
+                  done();
+                }); // END create table
+              }); // END test case
+            });*/
+
+            //9. Sort Order
+             /*describe('Sort Order function', function() {
+                it('Sort  Order', function(done) {
+                  var DatabaseManager = require('../lib/database/database-manager.js');
+                  DatabaseManager.sortorder(56,68,function(res) {
+                    if (res.status === false) {
+                      done(new Error(res.error.message));
+                      return;
+                    }
+                    done();
+                  }); // END create table
+                }); // END test case
+              });*/
+
+              //10. move node
+              /* describe('move node function', function() {
+                  it('move node', function(done) {
+                    var DatabaseManager = require('../lib/database/database-manager.js');
+                    DatabaseManager.movenode(56,68,function(res) {
+                      if (res.status === false) {
+                        done(new Error(res.error.message));
+                        return;
+                      }
+                      done();
+                    }); // END create table
+                  }); // END test case
+                });*/
+
 
 /*
   // Last. Check drop table
