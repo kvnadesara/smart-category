b63ba30957c236374447d9566bbc9873983c98a7
Kevin B. Adesara <kevin.adesara@comp91.digi-corp.com>
2016-05-16 19:23:18 +0530

    send assertion errors as web-service response

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var fs = require('fs');
 var config = require('./config.js');
+var assertMessageUtil = require('./lib/assert-message-util.js');
 
 app.use(compress());
 app.use(logger('dev'));
@@ -19,7 +20,9 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(session({
-  secret: 'Th!$!$$@mple'
+  secret: 'Th!$!$$@mple',
+  resave: true,
+  saveUninitialized: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
@@ -40,27 +43,49 @@ app.use(function(req, res, next) {
 
 // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+// development/production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  var assertionError = findAssertionMessageIfAvailable(err);
+  if(assertionError.hasOwnProperty('found') == false) {
     res.status(err.status || 500);
     res.send({
       message: err.message,
-      error: err
+      error: app.get('env') === 'development' ? err : {}
     });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.send({
-    message: err.message,
-    error: {}
-  });
+  } else {
+    // Found assertion message
+    res.status(200);
+    res.send(assertionError.error);
+  }
 });
+
+/**
+ * Find and prepare assertion validation error from lib_modules if available.
+ * Assert error message structure is ASSERT_<ERR_TYPE>_<VALUE1>_<VALUE2>
+ * @param  {error} err  Error   created from assertion
+ * @return {error/object}       Object {found: true, error: {code: <code>, message: <message>}}
+ *                                   if found as per above structure, else return error as is
+ */
+function findAssertionMessageIfAvailable(err) {
+  var errMessage = err.message;
+  var parsedAssertMessage = assertMessageUtil.parse(errMessage);
+  if(parsedAssertMessage == null) {
+    return err;
+  }
+
+  debug('actual assertion error: ', parsedAssertMessage);
+  return {
+    found: true,
+    error: {
+      status: false,
+      error: {
+        code: parsedAssertMessage.code,
+        message: parsedAssertMessage.message
+      }
+    }
+  };
+}
 
 
 module.exports = app;
